0a4bb0826ee43ec6a651e188eb2fb108b8298f99
mendgart444 <raffael.hauschildt@gmx.ch>
2025-03-26 19:47:26 +0100

    GXShell Version 1.0.0

---
+++ linux/release/build/proc-macro2-4567752d7f8dca8b/build_script_build-4567752d7f8dca8b.d
@@ -0,0 +1,5 @@
+E:\gxshell_release\linux\release\build\proc-macro2-4567752d7f8dca8b\build_script_build-4567752d7f8dca8b.exe: E:\.cargo\registry\src\index.crates.io-1949cf8c6b5b557f\proc-macro2-1.0.93\build.rs
+
+E:\gxshell_release\linux\release\build\proc-macro2-4567752d7f8dca8b\build_script_build-4567752d7f8dca8b.d: E:\.cargo\registry\src\index.crates.io-1949cf8c6b5b557f\proc-macro2-1.0.93\build.rs
+
+E:\.cargo\registry\src\index.crates.io-1949cf8c6b5b557f\proc-macro2-1.0.93\build.rs:
